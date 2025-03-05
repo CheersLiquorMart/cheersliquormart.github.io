1c9093ed5874167173bb2e895546a728feca45f2
lgizzle <128246707+lgizzle@users.noreply.github.com>
2025-03-05 06:55:05 -0700

    Use basic GitHub Pages setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://cheersliquormart.github.io',
-  base: '/',
   integrations: [tailwind()],
+  site: 'https://cheersliquormart.github.io',
+  base: '/cheersliquormart.github.io',
+  trailingSlash: 'always'
 });
