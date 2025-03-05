ec889085c8d468fa01d91dae52c883fc4533e410
CheersLiquorMart <les@cheersliquormart.com>
2025-03-04 21:55:38 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  base: "/cheersliquormart.github.io/",  // Set this to match your repo name
+  output: "static",  // Ensures Astro generates a static site
   integrations: [tailwind()],
 });
